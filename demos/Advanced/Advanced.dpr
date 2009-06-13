ab9be16954fc8c6d8f0aba69aed314a4dc0fe79f
mike.lischke@sun.com <mike.lischke@sun.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2009-06-13 13:28:56 +0000

    Added demos source code.

---
+++ demos/Advanced/Advanced.dpr
@@ -0,0 +1,28 @@
+program Advanced;
+
+{$R 'Res\ExtraRes.res' 'Res\ExtraRes.rc'}
+
+uses
+  Forms,
+  SpeedDemo in 'SpeedDemo.pas' {SpeedForm},
+  DrawTreeDemo in 'DrawTreeDemo.pas' {DrawTreeForm},
+  GeneralAbilitiesDemo in 'GeneralAbilitiesDemo.pas' {frmGeneralAbilities},
+  PropertiesDemo in 'PropertiesDemo.pas' {PropertiesForm},
+  GridDemo in 'GridDemo.pas' {GridForm},
+  Editors in 'Editors.pas',
+  VisibilityDemo in 'VisibilityDemo.pas' {VisibilityForm},
+  AlignDemo in 'AlignDemo.pas' {AlignForm},
+  Main in 'Main.pas' {MainForm},
+  WindowsXPStyleDemo in 'WindowsXPStyleDemo.pas' {WindowsXPForm},
+  MultilineDemo in 'MultilineDemo.pas' {NodeForm},
+  HeaderCustomDrawDemo in 'HeaderCustomDrawDemo.pas' {HeaderOwnerDrawForm},
+  States in 'States.pas' {StateForm};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.CreateForm(TStateForm, StateForm);
+  Application.Run;
+end.
