5121ac2fe13bb01ddfdeee161488a509f3705f6b
joachim.marder@gmail.com <joachim.marder@gmail.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2013-04-04 20:58:54 +0000

    V5.1.2

--- trunk/Source/VirtualTrees.pas
+++ trunk/Source/VirtualTrees.pas
@@ -1,6 +1,6 @@
 unit VirtualTrees;
 
-// Version 5.1.1
+// Version 5.1.2
 //
 // The contents of this file are subject to the Mozilla Public License
 // Version 1.1 (the "License"); you may not use this file except in compliance
