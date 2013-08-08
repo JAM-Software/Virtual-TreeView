2d1fe2999ff283a946fb21bfda99d3616286d781
joachim.marder@gmail.com <joachim.marder@gmail.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2013-08-08 20:15:01 +0000

    Implemented #238: OnMouseEnter and OnMouseLeave events missing

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -2405,6 +2405,8 @@ type
     FOnGetHintSize: TVTGetHintSizeEvent;
     fOnGetHintKind: TVTHintKindEvent;
     FOnIncrementalSearch: TVTIncrementalSearchEvent; // triggered on every key press (not key down)
+    FOnMouseEnter: TNotifyEvent;
+    FOnMouseLeave: TNotifyEvent;
 
     // operations
     FOnStartOperation: TVTOperationEvent;        // Called when an operation starts
@@ -2544,6 +2546,7 @@ type
     procedure CMFontChanged(var Message: TMessage); message CM_FONTCHANGED;
     procedure CMHintShow(var Message: TCMHintShow); message CM_HINTSHOW;
     procedure CMHintShowPause(var Message: TCMHintShowPause); message CM_HINTSHOWPAUSE;
+    procedure CMMouseEnter(var Message: TMessage); message CM_MOUSEENTER;
     procedure CMMouseLeave(var Message: TMessage); message CM_MOUSELEAVE;
     procedure CMMouseWheel(var Message: TCMMouseWheel); message CM_MOUSEWHEEL;
     procedure CMSysColorChange(var Message: TMessage); message CM_SYSCOLORCHANGE;
@@ -2711,6 +2714,8 @@ type
     function DoKeyAction(var CharCode: Word; var Shift: TShiftState): Boolean; virtual;
     procedure DoLoadUserData(Node: PVirtualNode; Stream: TStream); virtual;
     procedure DoMeasureItem(TargetCanvas: TCanvas; Node: PVirtualNode; var NodeHeight: Integer); virtual;
+    procedure DoMouseEnter(); virtual;
+    procedure DoMouseLeave(); virtual;
     procedure DoNodeCopied(Node: PVirtualNode); virtual;
     function DoNodeCopying(Node, NewParent: PVirtualNode): Boolean; virtual;
     procedure DoNodeClick(const HitInfo: THitInfo); virtual;
@@ -3019,6 +3024,8 @@ type
     property OnLoadNode: TVTSaveNodeEvent read FOnLoadNode write FOnLoadNode;
     property OnLoadTree: TVTSaveTreeEvent read FOnLoadTree write FOnLoadTree;
     property OnMeasureItem: TVTMeasureItemEvent read FOnMeasureItem write FOnMeasureItem;
+    property OnMouseEnter: TNotifyEvent read FOnMouseEnter write FOnMouseEnter;
+    property OnMouseLeave: TNotifyEvent read FOnMouseLeave write FOnMouseLeave;
     property OnNodeClick: TVTNodeClickEvent read FOnNodeClick write FOnNodeClick;
     property OnNodeCopied: TVTNodeCopiedEvent read FOnNodeCopied write FOnNodeCopied;
     property OnNodeCopying: TVTNodeCopyingEvent read FOnNodeCopying write FOnNodeCopying;
@@ -3718,6 +3725,8 @@ type
     property OnMouseMove;
     property OnMouseUp;
     property OnMouseWheel;
+    property OnMouseEnter;
+    property OnMouseLeave;
     property OnNewText;
     property OnNodeClick;
     property OnNodeCopied;
@@ -17973,6 +17982,13 @@ end;
 
 //----------------------------------------------------------------------------------------------------------------------
 
+procedure TBaseVirtualTree.CMMouseEnter(var Message: TMessage);
+begin
+  DoMouseEnter();
+end;
+
+//----------------------------------------------------------------------------------------------------------------------
+
 procedure TBaseVirtualTree.CMMouseLeave(var Message: TMessage);
 
 var
@@ -18004,7 +18020,7 @@ begin
     Header.FColumns.FHoverIndex := NoColumn;
     Header.FColumns.FCheckBoxHit := False;
   end;
-
+  DoMouseLeave();
   inherited;
 end;
 
@@ -20183,9 +20199,6 @@ end;
 
 procedure TBaseVirtualTree.ChangeScale(M, D: Integer);
 
-var
-  DoScale: Boolean;
-
 begin
   inherited;
 
@@ -22135,6 +22148,19 @@ begin
     FOnMeasureItem(Self, TargetCanvas, Node, NodeHeight);
 end;
 
+//----------------------------------------------------------------------------------------------------------------------
+
+procedure TBaseVirtualTree.DoMouseEnter();
+begin
+  if Assigned(FOnMouseEnter) then
+    FOnMouseEnter(Self);
+end;
+
+procedure TBaseVirtualTree.DoMouseLeave;
+begin
+  if Assigned(FOnMouseLeave) then
+    FOnMouseLeave(Self);
+end;
 //----------------------------------------------------------------------------------------------------------------------
 
 procedure TBaseVirtualTree.DoNodeCopied(Node: PVirtualNode);
