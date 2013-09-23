d9fa94c1104a2b11947e248eb3ca2092fb503091
joachim.marder@gmail.com <joachim.marder@gmail.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2013-09-23 22:03:24 +0000

    TBaseVirtualTree.SortTree() now immediately exits if there is just one or zero nodes.

--- trunk/Source/VirtualTrees.pas
+++ trunk/Source/VirtualTrees.pas
@@ -33414,6 +33414,8 @@ procedure TBaseVirtualTree.SortTree(Column: TColumnIndex; Direction: TSortDirect
   //--------------- end local function ----------------------------------------
 
 begin
+  if RootNode.TotalCount <= 2 then
+    exit;//Nothing to do if there are one or zero nodes. RootNode.TotalCount is 1 if there are no nodes in the treee as the root node counts too here.
   // Instead of wrapping the sort using BeginUpdate/EndUpdate simply the update counter
   // is modified. Otherwise the EndUpdate call will recurse here.
   Inc(FUpdateCount);
