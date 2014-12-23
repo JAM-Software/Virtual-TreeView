fe602970509d8a6517256aa88b6b564be00f750e
joachim.marder@gmail.com <joachim.marder@gmail.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2014-12-23 21:43:54 +0000

    Fixed #506: Autosort is being triggered at design-time

--- trunk/Source/VirtualTrees.pas
+++ trunk/Source/VirtualTrees.pas
@@ -10166,6 +10166,8 @@ var
   NewClickIndex: Integer;
 
 begin
+  if (csDesigning in Header.Treeview.ComponentState) then
+    exit;
   // Convert vertical position to local coordinates.
   Inc(P.Y, FHeader.FHeight);
   NewClickIndex := ColumnFromPosition(P);
