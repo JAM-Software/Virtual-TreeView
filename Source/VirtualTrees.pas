191203ef0f101b4dbb19f498011d9ac18ee85eda
joachim.marder@gmail.com <joachim.marder@gmail.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2015-01-09 12:15:53 +0000

    Improvements for toAlwaysSelectNode: We now also ensure in EndUpdate() that a node is selected, otherwise it could happen that no node was selected after initial filling although toAlwaysSelectNode was set.

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -28238,8 +28238,10 @@ begin
       end;
     end;
 
-    if FUpdateCount = 0 then
-      DoUpdating(usEnd)
+    if FUpdateCount = 0 then begin
+      DoUpdating(usEnd);
+      EnsureNodeSelected();
+    end
     else
       DoUpdating(usUpdate);
   end;
