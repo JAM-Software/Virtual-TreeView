9395a1e068307a060c8ce6511782c2a6bca38c47
obones <obones@free.fr>
2015-07-07 16:28:57 +0200

    FRoot.TotalNodeHeight can go below the default node height temporarily, especially when filtering nodes

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -32286,9 +32286,7 @@ procedure TBaseVirtualTree.UpdateVerticalRange;
 
 begin
   // Total node height includes the height of the invisible root node.
-  if FRoot.TotalHeight < FDefaultNodeHeight then
-    FRoot.TotalHeight := FDefaultNodeHeight;
-  FRangeY := FRoot.TotalHeight - FRoot.NodeHeight + FBottomSpace;
+  FRangeY := Cardinal(Int64(FRoot.TotalHeight) - FRoot.NodeHeight + FBottomSpace);
 end;
 
 //----------------------------------------------------------------------------------------------------------------------
