0c8b2e05e148ca1e3a7f27c6c4848fe3e520ee5b
obones <obones@free.fr>
2015-07-16 14:22:55 +0200

    As the new height is absolute, it should include the original height. In turn, this means that no special adjustment needs to be made when the parent node is root

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -14381,12 +14381,12 @@ begin
       if NewChildCount <> Node.ChildCount then
       begin
         InterruptValidation;
-        NewHeight := 0;
 
         if NewChildCount > Node.ChildCount then
         begin
           Remaining := NewChildCount - Node.ChildCount;
           Count := Remaining;
+          NewHeight := Node.TotalHeight;
 
           // New nodes to add.
           if Assigned(Node.LastChild) then
@@ -14421,11 +14421,7 @@ begin
           end;
 
           if vsExpanded in Node.States then
-          begin
-            if Node = FRoot then
-              Inc(NewHeight, Node.NodeHeight);
             AdjustTotalHeight(Node, NewHeight, False);
-          end;
 
           AdjustTotalCount(Node, Count, True);
           Node.ChildCount := NewChildCount;
