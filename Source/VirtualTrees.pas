2ed834eb8ef87dc072b06619d5410f2b84ff57e7
Joachim Marder <joachim.marder@gmail.com>
2015-11-18 11:28:57 +0100

    if only one node is selected, make sure the focused node changes with the selected node

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -17836,6 +17836,8 @@ begin
     Changed := InternalAddToSelection(FSingletonNodeArray, 1, False);
     if Changed then
     begin
+      if (SelectedCount = 1) then
+        FocusedNode := Node; // if only one node is selected, make sure the focused node changes with the selected node
       InvalidateNode(Node);
       Change(Node);
     end;
