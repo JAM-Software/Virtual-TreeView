5390e0b410b95c82fd9544da6634a6608795e3f5
Joachim Marder <joachim.marder@gmail.com>
2016-09-15 11:45:34 +0200

    Removed XE8+ unit "System.ImageList" from uses, which RAD Studio XE8+ adds automatically when saving.

--- Demos/Advanced/GeneralAbilitiesDemo.pas
+++ Demos/Advanced/GeneralAbilitiesDemo.pas
@@ -27,7 +27,7 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   StdCtrls, Buttons, VirtualTrees, ComCtrls, ExtCtrls, ImgList, Menus,
-  StdActns, ActnList, VTHeaderPopup, UITypes, System.ImageList;
+  StdActns, ActnList, VTHeaderPopup, UITypes;
 
 type
   TGeneralForm = class(TForm)
