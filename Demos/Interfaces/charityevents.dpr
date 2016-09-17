3d7fb4ddd13fff4a6b7b54a941d4eb7abacdef94
Joachim Marder <joachim.marder@gmail.com>
2016-09-17 12:33:33 +0200

    Turned on mem leak reporting

--- Demos/Interfaces/charityevents.dpr
+++ Demos/Interfaces/charityevents.dpr
@@ -9,6 +9,7 @@ uses
 {$R *.res}
 
 begin
+  ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFormModelView, FormModelView);
