c8b6d0466e79f6533ada68a9fb7928cff55a2304
Joachim Marder <joachim.marder@gmail.com>
2017-03-08 13:25:33 +0100

    Fixed issue #698 also for VCL styles.

--- Source/VirtualTrees.StyleHooks.pas
+++ Source/VirtualTrees.StyleHooks.pas
@@ -450,7 +450,9 @@ end;
 procedure TVclStyleScrollBarsHook.PaintScrollBars;
 begin
   FVertScrollBarWindow.Repaint;
+  RedrawWindow(FVertScrollBarWindow.Handle, nil, 0, RDW_FRAME or RDW_INVALIDATE); // Fixes issue #698
   FHorzScrollBarWindow.Repaint;
+  RedrawWindow(FHorzScrollBarWindow.Handle, nil, 0, RDW_FRAME or RDW_INVALIDATE); // Fixes issue #698
 end;
 
 function TVclStyleScrollBarsHook.PointInTreeHeader(const P: TPoint): Boolean;
