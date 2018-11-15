f980fbcdc47f2eaac65f1e4d4e3f45c6e362dafa
Joachim Marder <joachim.marder@outlook.com>
2018-11-15 21:22:20 +0100

    Set FCurrentTree to nil before calling ChangeTreeStatesAsync() to prevent a deadlock.

--- Source/VirtualTrees.WorkerThread.pas
+++ Source/VirtualTrees.WorkerThread.pas
@@ -184,10 +184,10 @@ begin
             EnterStates := [csUseCache];
 
         finally
-          LeaveStates := [csValidating, csStopValidation];
-          TBaseVirtualTreeCracker(FCurrentTree).ChangeTreeStatesAsync(EnterStates, LeaveStates);
           lCurrentTree := FCurrentTree; // Save reference in a local variable for later use
           FCurrentTree := nil; //Clear variable to prevent deadlock in CancelValidation. See #434
+          LeaveStates := [csValidating, csStopValidation];
+          TBaseVirtualTreeCracker(lCurrentTree).ChangeTreeStatesAsync(EnterStates, LeaveStates);
           Queue(TBaseVirtualTreeCracker(lCurrentTree).UpdateEditBounds);
         end;
       end;
