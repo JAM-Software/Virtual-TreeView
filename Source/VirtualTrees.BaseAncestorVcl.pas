277e8319d53227877fd6fc4f0a4b7e2a40366bf7
livius2 <liviuslivius@poczta.onet.pl>
2022-10-05 17:07:04 +0200

    [FMX port]Move IAccessibility properties to VCL
    Move IAccessibility properties to VCL

--- Source/VirtualTrees.BaseAncestorVcl.pas
+++ Source/VirtualTrees.BaseAncestorVcl.pas
@@ -13,16 +13,25 @@ unit VirtualTrees.BaseAncestorVCL;
 interface
 uses
   Winapi.Windows,
+  Winapi.oleacc,
   Winapi.ActiveX,
   Vcl.Controls;
 
 type
   TVTBaseAncestorVcl = class abstract(TCustomControl)
   private
-
+    // MSAA support
+    FAccessible: IAccessible;                    // The IAccessible interface to the window itself.
+    FAccessibleItem: IAccessible;                // The IAccessible to the item that currently has focus.
+    FAccessibleName: string;                     // The name the window is given for screen readers.
   protected
     function DoRenderOLEData(const FormatEtcIn: TFormatEtc; out Medium: TStgMedium; ForClipboard: Boolean): HRESULT; virtual; abstract;
     function RenderOLEData(const FormatEtcIn: TFormatEtc; out Medium: TStgMedium; ForClipboard: Boolean): HResult; virtual; abstract;
+    procedure NotifyAccessibilityCollapsed(); virtual; abstract;
+  public //properties
+    property Accessible: IAccessible read FAccessible write FAccessible;
+    property AccessibleItem: IAccessible read FAccessibleItem write FAccessibleItem;
+    property AccessibleName: string read FAccessibleName write FAccessibleName;
   end;
 
 implementation
