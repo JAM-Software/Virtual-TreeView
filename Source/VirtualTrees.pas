185558d7acf3bdc89745a1df5eaa2e4ef877de45
Joachim Marder <joachim.marder@outlook.com>
2022-10-28 08:50:41 +0200

    Revert "Issue #1136: Ensure 8 byte alignment for 64bit builds."
    This reverts commit 5eb28d0bd6bec3c0f107edee9963c1d5613de43f.

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -1371,8 +1371,6 @@ function TCustomVirtualStringTree.InternalData(Node: PVirtualNode): Pointer;
 begin
   if (Node = nil) or (FInternalDataOffset = 0) then
     Result := nil
-  else if Node = RootNode then
-    Result := PByte(Node) + FInternalDataOffset
   else
     Result := PByte(Node) + Self.NodeDataSize + FInternalDataOffset;
 end;
