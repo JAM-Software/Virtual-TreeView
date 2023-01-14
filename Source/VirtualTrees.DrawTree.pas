7e2951b6754ebee68ecc33b1d13e2af89e4827fb
Npc8 <13229506+Npc8@users.noreply.github.com>
2023-01-14 22:52:50 +0800

    Add column's checking and checked event

--- Source/VirtualTrees.DrawTree.pas
+++ Source/VirtualTrees.DrawTree.pas
@@ -156,6 +156,8 @@ type
     property OnClick;
     property OnCollapsed;
     property OnCollapsing;
+    property OnColumnChecked;
+    property OnColumnChecking;
     property OnColumnClick;
     property OnColumnDblClick;
     property OnColumnExport;
