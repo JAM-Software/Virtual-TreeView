db238f6bb3e68e1b5088e240a3466d1ea9d45502
Joachim Marder <joachim.marder@outlook.com>
2023-08-13 11:59:36 +0200

    Issue #1206: TVTHeader.AutoScale() never decreases height

--- Source/VirtualTrees.Header.pas
+++ Source/VirtualTrees.Header.pas
@@ -639,8 +639,6 @@ begin
       finally
         Free;
       end;
-    //Get the maximum of the scaled original value and the minimum needed header height.
-    lMaxHeight := Max(lMaxHeight, FHeight);
     //Set the calculated size
     Self.SetHeight(lMaxHeight);
   end;
